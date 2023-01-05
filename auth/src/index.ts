62c3db00ec1b7ff720c16147a68b3664dcacfb7b
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-05 18:39:08 +0200

    Section 7

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,32 @@
-import express from 'express';
+import express,{Request,response} from 'express';
+import 'express-async-errors'
 import {json} from 'body-parser';
+
+//Routes
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signupRouter} from './routes/signup';
+
+
+// Error Handlers 
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
+app.use(signinRouter);
+app.use(currentUserRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!');
 });
