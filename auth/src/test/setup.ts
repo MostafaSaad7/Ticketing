abdf7bd80943fcd8a843ad3daa121b13a63fef6d
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-09 18:44:09 +0200

    Setup test environment 	modified:   auth/package.json 	new file:   auth/src/test/setup.ts

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,32 @@
+import mongoose, { Collection, mongo } from "mongoose";
+import {MongoMemoryServer} from "mongodb-memory-server"
+import { app } from "../app";
+let mongo:any;
+beforeAll(async ()=>{
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+    console.log(mongoUri);
+    await mongoose.connect(mongoUri, {});
+
+});
+
+
+beforeEach(async ()=>{
+
+
+    const collections = await mongoose.connection.db.collections();
+
+    for(let collection of collections)
+    {
+        await collection.deleteMany({});
+    }
+
+
+
+});
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
