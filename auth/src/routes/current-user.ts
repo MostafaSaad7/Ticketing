8fb385cbf81d52fab4485a072228724bc46b78dd
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-09 17:49:23 +0200

    Create current user middleware and require auth middleware 	new file:   auth/src/errors/not-authorized-error.ts 	new file:   auth/src/middlewares/current-user.ts 	new file:   auth/src/middlewares/require-auth.ts 	modified:   auth/src/routes/current-user.ts 	modified:   auth/src/routes/signout.ts

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,14 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import  Jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req:Request, res:Response) => {
-  if(!req.session?.jwt){ // if there is no session or no jwt
-   res.send({currentUser: null});
-  }
-  try{
-    const payload = Jwt.verify(req.session!.jwt, process.env.JWT_KEY!);
-    res.send({currentUser: payload});
-  }
-  catch(err){
-    res.send({currentUser: null});
-  }
+router.get('/api/users/currentuser', currentUser,(req:Request, res:Response) => {
+
+  res.send({ currentUser: req.currentUser || null });
+
+  
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -4,7 +4,9 @@ import { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req:Request, res:Response) => {
-  res.send("Hi there!");
+  req.session = null;
+  res.send({});
+
 });
 
 export { router as signoutRouter };
