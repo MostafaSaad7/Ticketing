082f0454ad937108ccd5db9830972ab67bf461e5
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-10 21:01:33 +0200

    additional config 	new file:   common/.gitignore 	new file:   common/package-lock.json 	modified:   common/package.json 	new file:   common/src/index.ts 	new file:   common/tsconfig.json

---
+++ common/src/index.ts
@@ -0,0 +1,14 @@
+interface color{
+    red:number;
+    blue:number;
+    green:number;
+}
+
+const color:color = {
+    red: 255,
+    blue: 255,
+    green: 255
+};
+console.log(color);
+
+export default color;
