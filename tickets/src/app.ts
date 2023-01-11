2731a083ad00791a3f908ff8fc7b2895413b72bc
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-11 16:28:29 +0200

    Untracked files issue resolved to fix .gitignore

---
+++ tickets/src/app.ts
@@ -0,0 +1,28 @@
+import express,{Request,response} from 'express';
+import 'express-async-errors'
+import {json} from 'body-parser';
+import cookieSession from 'cookie-session';
+
+
+
+
+
+
+// Error Handlers 
+import { errorHandler,NotFoundError } from '@ms-shared-ticketing/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !=='test'
+}));
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app} ;
