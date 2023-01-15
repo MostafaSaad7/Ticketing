1a3a79feff58591fc477350d3ab6160fab242024
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-15 18:10:21 +0200

    Create NATS queue group for listener 	modified:   nats-test/src/listener.ts

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,18 @@
 import nats,{Message} from 'node-nats-streaming';
-
+import {randomBytes} from 'crypto';
 
 console.clear();
 
+const clientId=randomBytes(4).toString('hex');
 
-const stan=nats.connect('ticketing','123',{
+const stan=nats.connect('ticketing',clientId,{
     url:'http://localhost:4222'
 });
 
 
 stan.on('connect',()=>{
     console.log('Listener connected to NATS');
-    const subscription=stan.subscribe('ticket:created'); // subscribe to channel (topic) ticket:created
+    const subscription=stan.subscribe('ticket:created','orders-service-queue-group'); // subscribe to channel (topic) ticket:created
     subscription.on('message',(msg:Message)=>{
         console.log('Message received');
         console.log(`Received event # ${msg.getSequence()} with message data: ${msg.getData()}`);
