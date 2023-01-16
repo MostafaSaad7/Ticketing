e815e079b4039db3ab22bcabaf2fb9e125ec9f42
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-16 08:34:44 +0200

    New Updates to package

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -1,15 +1,10 @@
 import { Subjects } from "./subjects";
-
-
-export interface TicketCreatedEvent {
-
-    subject: Subjects.TicketCreated;
+export interface TicketUpdatedEvent {
+    subject: Subjects.TicketUpdated;
     data: {
         id: string;
         title: string;
         price: number;
         userId: string;
     };
-
-
-}
+}   
