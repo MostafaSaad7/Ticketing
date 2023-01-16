701c4c03ba711216fef26863310ea18777d8c758
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-16 08:28:25 +0200

    Implement base Publisher and Events in Common module 	new file:   common/src/events/base-listener.ts 	new file:   common/src/events/base-publisher.ts 	new file:   common/src/events/subjects.ts 	new file:   common/src/events/ticket-created-event.ts 	modified:   nats-test/src/events/base-listener.ts 	new file:   nats-test/src/events/base-publisher.ts 	modified:   nats-test/src/events/subjects.ts 	modified:   nats-test/src/events/ticket-created-listener.ts 	new file:   nats-test/src/events/ticket-created-publisher.ts 	modified:   nats-test/src/publisher.ts

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,15 @@
 
 import { Listener } from "./base-listener";
 import { Message } from "node-nats-streaming";
-export class TicketCreatedListener extends Listener {
-    subject: string = 'ticket:created';
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
+
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName: string = 'payments-service';
 
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         console.log(`Received event # ${msg.getSequence()} with message data `);
         console.log(data);
 
