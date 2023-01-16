d05a7dcd8602b3cb332e30e2905ea2b91cd52e59
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-16 23:40:50 +0200

    modified:   tickets/package-lock.json 	modified:   tickets/package.json 	modified:   tickets/src/index.ts 	new file:   tickets/src/nats-wrapper.ts 	modified:   tickets/src/routes/new.ts

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,21 +1,20 @@
 import mongoose from 'mongoose';
 import { app } from './app';
-const start= async()=>{
-    if(process.env.JWT_KEY=== 'test')
-    {
+import { natsWrapper } from './nats-wrapper';
+const start = async () => {
+    if (process.env.JWT_KEY === 'test') {
         throw new Error('JWT_KEY must be defined');
     }
-    if(!process.env.MONGO_URI)
-    {
+    if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
-    try{
-        
-    
-   await mongoose.connect(process.env.MONGO_URI!);
+    try {
 
-    } 
-    catch(err){
+        await natsWrapper.connect('ticketing', 'asdf', 'http://nats-srv:4222');
+        await mongoose.connect(process.env.MONGO_URI!);
+
+    }
+    catch (err) {
         console.error(err);
     }
     app.listen(3000, () => {
