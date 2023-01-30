d9a0f280373dd50b6dd75784a4afea6e08e93468
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-30 23:49:12 +0200

    Create unit test for index route and new route 	new file:   orders/src/__mocks__/nats-wrapper.ts 	new file:   orders/src/routes/__test__/index.test.ts 	new file:   orders/src/routes/__test__/new.test.ts 	modified:   orders/src/routes/index.ts

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@ms-shared-ticketing/common';
+import { Order } from '../models/order';
+
 
 const router = express.Router();
 
-router.get('/api/orders', (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser!.id,
+    }).populate('ticket');
+
+    res.send(orders);
 });
 
-
 export { router as indexOrderRouter };
+
+
