5c838e93e05dc59aae8c0b62439af9a10aa6f220
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-31 08:59:27 +0200

    Publish order created event 	modified:   orders/src/routes/new.ts

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -3,6 +3,8 @@ import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateReque
 import { body } from 'express-validator';
 import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 
 const router = express.Router();
@@ -53,6 +55,16 @@ router.post('/api/orders', requireAuth,
 
 
         // Publish an event saying that an order was created
+        new OrderCreatedPublisher(natsWrapper.client).publish({
+            id: order.id,
+            status: order.status,
+            userId: order.userId,
+            expiresAt: order.expiresAt.toISOString(),
+            ticket: {
+                id: ticket.id,
+                price: ticket.price
+            }
+        });
 
 
 
