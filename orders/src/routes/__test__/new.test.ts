1652888cbe94eebb4232dceb56f9a6d77be3303a
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-01-31 09:13:15 +0200

    Test Order service Event Publisher 	modified:   orders/src/routes/__test__/delete.test.ts 	modified:   orders/src/routes/__test__/new.test.ts

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,8 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
+
 
 
 
@@ -54,4 +56,22 @@ it('reserves a ticket', async () => {
 
 });
 
-it.todo('emits an order created event');
+it('emits an order created event', async () => {
+
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+}
+
+);
