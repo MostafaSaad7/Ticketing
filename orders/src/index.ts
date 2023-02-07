b42e81870445dc00f0e51a196bd2a135ef6e1c22
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-07 12:41:20 +0200

    edit orders index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,7 +6,7 @@ import { TicketCreatedListener } from './events/listeners/ticket-created-listene
 
 
 const start = async () => {
-    console.log('Starting....');
+    console.log('Starting......');
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
