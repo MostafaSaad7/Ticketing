64ba8c4a9d16c3ad51a992d69234d61f78b3c462
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-16 06:01:47 +0200

    Solve Cluster ID issue in expiration service deployment file

--- expiration/src/nats-wrapper.ts
+++ expiration/src/nats-wrapper.ts
@@ -14,7 +14,7 @@ class NatsWrapper {
         this._client = nats.connect(clusterId, clientId, { url });
 
         return new Promise<void>((resolve, reject) => {
-            
+
             // this.client is a getter property
             this.client.on('connect', () => {
                 console.log('Connected to NATS');
@@ -32,3 +32,4 @@ class NatsWrapper {
 }
 
 export const natsWrapper = new NatsWrapper();
+//
