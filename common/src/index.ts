a68c386b0ec2dd59d3e3945b7fa3eaf39d90cd67
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-16 07:33:03 +0200

    Add expiration complete event interface to common module 	new file:   common/src/events/expiration-complete-event.ts 	modified:   common/src/events/subjects.ts 	modified:   common/src/index.ts

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,6 @@
 // This file is used to export all the files in the common folder
 
+
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
@@ -15,11 +16,15 @@ export * from './middlewares/validate-request';
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
+
+// Ticket service events
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
+
+// Order service events
 export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
-
-
 export * from './events/types/order-status';
 
+// Expiration service events
+export * from './events/expiration-complete-event';
