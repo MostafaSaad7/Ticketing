277cc81b4082cedbc982f076cdb46f61ca415437
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-16 07:29:20 +0200

    Scheduled job to publish expiration:complete event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -11,10 +11,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log(new Date(data.expiresAt).getTime());
+        console.log('the delay is:', delay);
+        console.log("Waiting this many milliseconds to process the job:", delay);
         await expirationQueue.add({
             orderId: data.id
-        });
+        }, {
+            delay
+        }
+        );
         msg.ack();
 
 
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -23,6 +23,7 @@ const start = async () => {
         });
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
+
         new OrderCreatedListener(natsWrapper.client).listen();
 
 
