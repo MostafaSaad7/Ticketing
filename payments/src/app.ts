87a65530ba9dc7c1347a88f04aced5b94cbd299e
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-19 09:12:25 +0200

    Create a new route for payments service

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,15 +1,15 @@
-import express,{Request,response} from 'express';
+import express, { Request, response } from 'express';
 import 'express-async-errors'
-import {json} from 'body-parser';
+import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import {currentUser} from '@ms-shared-ticketing/common';
+import { currentUser } from '@ms-shared-ticketing/common';
 
 // Error Handlers 
-import { errorHandler,NotFoundError } from '@ms-shared-ticketing/common';
-
- 
+import { errorHandler, NotFoundError } from '@ms-shared-ticketing/common';
 
 
+// Routes
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 
@@ -20,17 +20,19 @@ app.use(json());
 
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !=='test'
+    secure: process.env.NODE_ENV !== 'test'
 }));
 
 app.use(currentUser);
 
+// Routes
+app.use(createChargeRouter);
 
 
- 
+
 app.all('*', async (req, res) => {
     throw new NotFoundError();
 });
 app.use(errorHandler);
 
-export {app} ;
+export { app };
---
+++ payments/src/routes/new.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest, BadRequestError, NotFoundError } from '@ms-shared-ticketing/common';
+import { Order } from '../models/orders';
+
+
+const router = express.Router();
+
+
+router.post('/api/payments', requireAuth, [
+    body('token').not().isEmpty(),
+    body('orderId').not().isEmpty()
+], async (req: Request, res: Response) => {
+    res.send({ success: true });
+});
+
+
+export { router as createChargeRouter };
