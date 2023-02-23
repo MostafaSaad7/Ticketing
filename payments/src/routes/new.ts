589b3baac80b0e8fa7396e96eb10ecf00048a803
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2023-02-23 10:40:24 +0200

    Add payment created object to new payment route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,8 +4,12 @@ import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthor
 import { Order } from '../models/orders';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payments';
-const router = express.Router();
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
+
+
+const router = express.Router();
 
 router.post('/api/payments', requireAuth, [
     body('token').not().isEmpty(),
@@ -39,8 +43,15 @@ router.post('/api/payments', requireAuth, [
 
     await payment.save();
 
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripeId: payment.stripeId
+    });
 
-    res.status(201).send({ success: true });
+
+    res.status(201).send({ id: payment.id });
+
 });
 
 
